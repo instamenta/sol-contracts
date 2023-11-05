013be5d93c6e50a007ef816f89ca164f4ff17dc7
Jan Milenkov <instamenta@abv.bg>
2023-11-05 19:11:18 +0200

    Update HexaCoin.sol

--- HexaCoin.sol
+++ HexaCoin.sol
@@ -1,3 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
+contract HexaCoin is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+    uint256 private _nextTokenId;
+    uint256 public constant MAX_SUPPLY = 10000;
 
+    constructor(address initialOwner)
+        ERC721("HexaCoin", "HEXA")
+    {}
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        require(_nextTokenId < MAX_SUPPLY, "Max token supply reached");
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Enumerable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(address account, uint128 value)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._increaseBalance(account, value);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
